51c8838c1c5e0e9b6a3a02988fe285de088085d1
HARIHULK <harikrishnabiyyala@gmail.com>
2020-08-02 23:10:38 +0530

    Updates

---
+++ precache-manifest.47b244166d53471e3019e70773ddcfe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d1b01328e1b8b2e8a842846d14eb23",
+    "url": "/prod/index.html"
+  },
+  {
+    "revision": "d938d169b6c9539a7e0d",
+    "url": "/prod/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "62a2eed5d30b295e8b16",
+    "url": "/prod/static/js/2.466ff0d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prod/static/js/2.466ff0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d938d169b6c9539a7e0d",
+    "url": "/prod/static/js/main.04d099ed.chunk.js"
+  },
+  {
+    "revision": "685c192e76d34023b30c",
+    "url": "/prod/static/js/runtime-main.a3730764.js"
+  }
+]);
